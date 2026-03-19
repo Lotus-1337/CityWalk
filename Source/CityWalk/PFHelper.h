77050c3c80bb1bac913477c6f0c9607622d917bd
Lotus-1337 <albus7922@gmail.com>
2026-03-19 20:28:08 +0100

    PF Helper.h
     File Containing Helpful Structures and functions

---
+++ Source/CityWalk/PFHelper.h
@@ -0,0 +1,98 @@
+#pragma once
+
+#include "CoreMinimal.h"
+
+#include "Detour/DetourNavMesh.h"
+
+#include "PFHelper.generated.h"
+
+struct dtMeshTile;
+struct dtPoly;
+
+USTRUCT()
+struct FPolyHandle
+{
+	GENERATED_BODY()
+
+	dtPolyRef Ref;
+	const dtPoly* Poly;
+
+	FPolyHandle()
+	{
+		Ref = 0;
+		Poly = nullptr;
+	}
+
+	FPolyHandle(const dtPolyRef& nRef, const dtPoly* nPoly)
+	{
+		Ref = nRef;
+		Poly = nPoly;
+	}
+
+	FORCEINLINE bool IsRefValid() const { return Ref != 0; };
+
+	FORCEINLINE bool IsPolyValid() const { return Poly != nullptr; };
+
+	FORCEINLINE bool IsValidBoth() const { return IsRefValid() && IsPolyValid(); };
+
+};
+
+/** Struct Containing Tile Info For Looking For Polys **/
+USTRUCT()
+struct FPolyInfo
+{
+
+	GENERATED_BODY()
+
+	FPolyHandle MainHandle;
+	FPolyHandle OtherHandle;
+
+	const dtMeshTile* Tile;
+
+	const dtNavMesh* Mesh;
+
+	FPolyInfo()
+	{
+
+		MainHandle  = FPolyHandle();
+		OtherHandle = FPolyHandle();
+
+		Tile = nullptr;
+		Mesh = nullptr;
+	}
+
+	// The 'n' prefix stands for 'New', to avoid using this-> or more confusing names
+	FPolyInfo(const FPolyHandle & nMainHandle, const FPolyHandle & nOtherHandle, const dtMeshTile* nTile, const dtNavMesh* nMesh)
+	{
+
+		MainHandle  = nMainHandle;
+		OtherHandle = nOtherHandle;
+
+		Tile = nTile;
+		Mesh = nMesh;
+	}
+
+	/**
+	* Checks If Any Pointer contained by this struct is Invalid. *
+	*/
+	FORCEINLINE bool IsValid() const
+	{
+		return MainHandle.IsBothValid && OtherHandle.IsBothValid() && != nullptr && Tile != nullptr && Mesh != nullptr;
+	}
+
+};
+
+// Converting From Detour to UE5.
+FORCEINLINE FVector RealToVector(const dtReal* V)
+{
+	// X -> X, Z -> Y, Y -> Z
+	return FVector(V[0], V[2], V[1]);
+}
+
+FORCEINLINE void VectorToReal(const FVector& V, dtReal Out[3])
+{
+	// X -> X, Y -> Z, Z -> Y
+	Out[0] = V.X;
+	Out[1] = V.Z;
+	Out[2] = V.Y;
+}
