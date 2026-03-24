8e98289e3f45e600664e5d6e6c173b9f1857a7ef
Lotus-1337 <albus7922@gmail.com>
2026-03-24 19:38:29 +0100

    Some Logic Changes, It's temporary

--- Source/CityWalk/PathFinder.cpp
+++ Source/CityWalk/PathFinder.cpp
@@ -340,7 +340,14 @@ void APathFinder::GetNeighbours(TArray<FPolyNode>& NeighboursArr, dtPolyRef* Pol
 			Neighbour = &PolyInfo.Tile->polys[NeighbourIndex - 1];
 		}
 
+		PolyInfo.OtherHandle = FPolyHandle(0, Neighbour);
+
+
 		NeighbourRef = GetPolyRef(PolyInfo, EWhichHandle::OTHER);
+
+		if (NeighbourRef == 0) continue;
+
+		PolyInfo.OtherHandle.Ref = NeighbourRef;
 
 		FPortal Portal = FPortalBuilder::BuildPortal(&PolyInfo); // Uncomment as soon as BuildPortal is implemented 
 		FPolyNode NeighbourPolyNode = FPolyNode();
