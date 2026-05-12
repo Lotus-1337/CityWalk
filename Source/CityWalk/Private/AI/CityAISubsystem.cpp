4b082deffcb6adfb7e4e8fcd379e90694d572313
Lotus-1337 <albus7922@gmail.com>
2026-05-12 20:43:14 +0200

    Implemented AI Managment Subsystem
    Will Contain LOD Manager in the future too.

---
+++ Source/CityWalk/Private/AI/CityAISubsystem.cpp
@@ -0,0 +1,63 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AI/CityAISubsystem.h"
+
+#include "AI/AIActor.h"
+#include "Player/WalkerCharacter.h"
+
+// Sets default values
+UCityAISubsystem::UCityAISubsystem()
+{
+
+	FString Path = TEXT("/Game/Blueprints/");
+
+	FString AIClassPath = Path / TEXT("BP_AIActor.BP_AIActor_C");
+
+	static ConstructorHelpers::FClassFinder<AAIActor> AIClassAsset(*AIClassPath);
+
+	if (!AIClassAsset.Succeeded())
+	{
+		return;
+	}
+
+	AIClass = AIClassAsset.Class;
+
+}
+
+void UCityAISubsystem::OnWorldBeginPlay(UWorld& World)
+{
+
+	Super::OnWorldBeginPlay(World);
+
+}
+
+void UCityAISubsystem::SpawnAI()
+{
+
+	const int32 MaxAI = 32;
+	const int32 MaxSpawnedAIPerFrame = 16;
+
+	const int32 MaxAIThisFrame = FMath::Clamp(MaxAI - AIArray.Num(), 0, MaxSpawnedAIPerFrame);
+
+	const FVector MaxAIPositionRange = FVector(1000.0f, 1000.0f, 0.0f);
+	const FVector PlayerLocation = Player->GetActorLocation();
+
+	FVector Min = PlayerLocation - MaxAIPositionRange;
+	FVector Max = PlayerLocation + MaxAIPositionRange;
+
+	FActorSpawnParameters Params;
+	Params.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AdjustIfPossibleButAlwaysSpawn;
+
+	for (int32 i = 0; i < MaxAIThisFrame; i++)
+	{
+
+		FVector RandomLocation = GetRandomVector(Min.X, Max.X, Min.Y, Max.Y, MaxAIPositionRange.Z);
+
+		AAIActor* AI = GetWorld()->SpawnActor<AAIActor>(AIClass, RandomLocation, FRotator::ZeroRotator, Params);
+
+		AIArray.Add(AI);
+
+	}
+
+}
---
+++ Source/CityWalk/Public/AI/CityAISubsystem.h
@@ -0,0 +1,47 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/WorldSubsystem.h"
+#include "CityAISubsystem.generated.h"
+
+class AAIActor;
+class AWalkerCharacter;
+
+UCLASS()
+class CITYWALK_API UCityAISubsystem : public UWorldSubsystem
+{
+	GENERATED_BODY()
+
+protected:
+
+	UPROPERTY(EditDefaultsOnly, Category = "AI")
+	TArray<AAIActor*> AIArray;
+
+	UPROPERTY(EditDefaultsOnly, Category = AI)
+	TSubclassOf<AAIActor> AIClass;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Player")
+	AWalkerCharacter* Player;
+
+public:	
+	// Sets default values for this actor's properties
+	UCityAISubsystem();
+
+protected:
+
+	// Called when the game starts or when spawned
+	virtual void OnWorldBeginPlay(UWorld& World) override;
+
+public:	
+
+
+	void SpawnAI();
+
+	FORCEINLINE void SetPlayer(AWalkerCharacter* NewPlayer)
+	{
+		Player = NewPlayer;
+	}
+
+};
